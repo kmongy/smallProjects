b6b25e75ed5423faef3ba1719d611f673d192f05
Kong Yang <kmongy05@gmail.com>
2020-05-26 13:25:34 -0500

    Added toggle logic to add and remove the open css class on click. Also closing all the rest of the open tags when one is clicked.

--- accordion/src/main.js
+++ accordion/src/main.js
@@ -1,1 +1,18 @@
-console.log('WORKS!');
+const accordion = document.querySelector('.accordion');
+const itemsForDropdown = accordion.querySelectorAll('.accordion-wrapper');
+const accordionHeading = accordion.querySelectorAll('.accordion__header');
+
+function toggleText() {
+  const thisItem = this.parentNode;
+
+  itemsForDropdown.forEach(item => {
+    if (thisItem === item) {
+      thisItem.classList.toggle('open');
+      return;
+    }
+
+    item.classList.remove('open');
+  });
+}
+
+accordionHeading.forEach(dropdownItem => dropdownItem.addEventListener('click', toggleText))

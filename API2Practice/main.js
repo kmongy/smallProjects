5f1b3bc5c4d34508542c258bcdb5598679d9263d
Kong Yang <kmongy05@gmail.com>
2020-05-28 14:36:59 -0500

    Practice with an actual api. No key though.

--- API2Practice/main.js
+++ API2Practice/main.js
@@ -1,9 +1,4 @@
-console.log('Fetching a picture');
+const url = 'https://randomuser.me/api/';
+const randomUsersUL = document.querySelector('.random-users');
 
-fetch('me-Small.jpg')
-  .then(response => response.blob()) // need the response to convert to something that's readable - in this case, a blob for an img. Theres' JSON, etc.
-  .then(blob => {
-    console.log(blob);
-    document.getElementById('pictureOfMe').src = URL.createObjectURL(blob); // now get we can use the blob.
-  })
-  .catch(error => console.log(error));
+console.log('test');

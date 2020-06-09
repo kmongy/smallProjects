3e11d52b11873f7369085028780cf79b9e57ee0f
Kong Yang <kmongy05@gmail.com>
2020-06-09 15:40:57 -0500

    Updated with some js logic for the switching.

--- showPassword/src/main.js
+++ showPassword/src/main.js
@@ -1,1 +1,18 @@
-console.log('I WORK!')
+const form = document.querySelector('.password-form');
+const passwordField = document.querySelector('.password-field');
+const passwordInput = passwordField.querySelector('#password');
+const revealPasswordBtn = document.querySelector('.reveal-password-button');
+const formSubmit = document.querySelector('.form-submit');
+
+function revealPassword(event) {
+  event.preventDefault();
+
+  if(passwordInput.type === 'password') {
+    passwordInput.type = 'text';
+  } else if(passwordInput.type === 'text') {
+    passwordInput.type = 'password';
+  }
+}
+
+revealPasswordBtn.addEventListener('click', revealPassword);
+formSubmit.addEventListener('click', e => e.preventDefault());

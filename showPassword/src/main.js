75711ece57190dc94a066a5e3fd8279bdf2282a4
Kong Yang <kmongy05@gmail.com>
2020-06-09 15:44:33 -0500

    updated with a icon change for the eye on click.

--- showPassword/src/main.js
+++ showPassword/src/main.js
@@ -3,14 +3,17 @@ const passwordField = document.querySelector('.password-field');
 const passwordInput = passwordField.querySelector('#password');
 const revealPasswordBtn = document.querySelector('.reveal-password-button');
 const formSubmit = document.querySelector('.form-submit');
+const eyeButton = document.querySelector('.fa-eye');
 
 function revealPassword(event) {
   event.preventDefault();
 
   if(passwordInput.type === 'password') {
     passwordInput.type = 'text';
+    eyeButton.className = 'far fa-eye-slash';
   } else if(passwordInput.type === 'text') {
     passwordInput.type = 'password';
+    eyeButton.className = 'far fa-eye';
   }
 }
 

71fd1f78dc8d6e42e07f14cc00c551b011d6bc0c
Kong Yang <kmongy05@gmail.com>
2020-06-10 14:55:59 -0500

    forgot about the show password part. Added it in html and fixed css.

--- formValidation/src/validation.js
+++ formValidation/src/validation.js
@@ -1,1 +1,17 @@
-console.log('test');
+const mainForm = document.querySelector('.main-form');
+const username = document.querySelector('#username');
+const email = document.querySelector('#email');
+const formPassword = document.querySelector('#password');
+const passwordCheck = document.querySelector('#password-check');
+const formButton = document.querySelector('.form-submit-button');
+
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function submitForm(event) {
+  event.preventDefault();
+}
+
+formButton.addEventListener('click', submitForm);

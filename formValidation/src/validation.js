46d768de728b4b3f2016152c3b69aa69c4499214
Kong Yang <kmongy05@gmail.com>
2020-06-10 15:26:28 -0500

    Adding some logic to the js. Created functions for various states.

--- formValidation/src/validation.js
+++ formValidation/src/validation.js
@@ -3,15 +3,42 @@ const username = document.querySelector('#username');
 const email = document.querySelector('#email');
 const formPassword = document.querySelector('#password');
 const passwordCheck = document.querySelector('#password-check');
+
 const formButton = document.querySelector('.form-submit-button');
+const revealPasswordBtn = document.querySelector('.reveal');
+const revealPasswordCheckBtn = document.querySelector('.reveal-check');
+
+function submitForm(event) {
+  event.preventDefault();
+
+  checkRequired([username, email, formPassword, passwordCheck]);
+}
+
+function error(input, message) {
+  const formErrorHandling = input.parentElement;
+  const errorMessageSpan = formErrorHandling.querySelector('.error-message');
+
+  formErrorHandling.className = 'inner-form-wrapper error';
+  errorMessageSpan.innerText = message;
+}
+
+function checkRequired(inputArray) {
+  inputArray.forEach((input) => {
+    if (input.value.trim() === '') {
+      error(input, `${input.id} is required`);
+    }
+  });
+}
 
 function validateEmail(email) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return re.test(String(email).toLowerCase());
 }
 
-function submitForm(event) {
+function revealPassword(event) {
   event.preventDefault();
 }
 
-formButton.addEventListener('click', submitForm);
+mainForm.addEventListener('submit', submitForm);
+revealPasswordBtn.addEventListener('click', revealPassword);
+revealPasswordCheckBtn.addEventListener('click', revealPassword);

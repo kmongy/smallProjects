3d991968f26a2029418825d0df73602d5aea6afb
Kong Yang <kmongy05@gmail.com>
2020-06-22 17:43:08 -0500

    Updated with more logic and to display the values to the dom.

--- degreeConversion/convert.js
+++ degreeConversion/convert.js
@@ -3,25 +3,43 @@ const formBtn = tempForm.querySelector('.calculate');
 const initialFahrenheit = tempForm.querySelector('#fahrenheit-input');
 const initialCelsius = tempForm.querySelector('#celsius-input');
 
+const fahrenheitOutput = document.querySelector('.fahrenheit-to-celsius');
+const celsiusOutput = document.querySelector('.celsius-to-fahrenheit');
+
 function calculateDegrees(event) {
   event.preventDefault();
 
-  convertToCelsius();
-  convertToFahrenheit();
+  if (initialFahrenheit.value) {
+    convertToCelsius();
+  } else if (initialFahrenheit.value === '') {
+    console.log('test');
+  }
+
+  if (initialCelsius.value) {
+    convertToFahrenheit();
+  } else if (initialCelsius.value === '') {
+    console.log('test');
+  }
 }
 
-function convertToFahrenheit(celsius, fahrenheit) {
-  fahrenheit = initialFahrenheit.value;
+function convertToFahrenheit(celsius) {
   celsius = initialCelsius.value;
+  let conversion = (celsius * 1.8 + 32).toFixed(0);
 
-  console.log((celsius * 1.8 + 32).toFixed(0));
+  fahrenheitOutput.innerHTML = `
+  <p>Your Celsius value was ${celsius}</p>
+  <p class="fahrenheit-output">${conversion}&#176; Fahrenheit</p>
+  `;
 }
 
-function convertToCelsius(fahrenheit, celsius) {
+function convertToCelsius(fahrenheit) {
   fahrenheit = initialFahrenheit.value;
-  celsius = initialCelsius.value;
+  let conversion = ((fahrenheit - 32) / 1.8).toFixed(0);
 
-  console.log(((fahrenheit - 32) / 1.8).toFixed(0));
+  celsiusOutput.innerHTML = `
+  <p>Your Fahrenheit value was ${fahrenheit}</p>
+  <p class="celsius-output">${conversion}&#176; Celsius</p>
+  `;
 }
 
 tempForm.addEventListener('submit', calculateDegrees);

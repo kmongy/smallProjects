8857133a3ae8a572e7d5a4e8c16effd223da769b
Kong Yang <kmongy05@gmail.com>
2020-06-29 12:27:24 -0500

    updated with a quick little modal project.

---
+++ modal/modal.js
@@ -0,0 +1,39 @@
+const cardButtons = document.querySelectorAll('.card button');
+const modalOuter = document.querySelector('.modal-outer');
+const modalInner = document.querySelector('.modal-inner');
+
+function handleCardButtonClick(event) {
+  const button = event.currentTarget;
+  const card = button.closest('.card');
+  const imgSrc = card.querySelector('img').src;
+  const description = card.dataset.description;
+  const name = card.querySelector('h2').textContent;
+
+  modalInner.innerHTML = `
+    <img src="${imgSrc.replace('300', '600')}" alt="${name}">
+    <p>${description}</p>
+  `;
+
+  modalOuter.classList.add('open');
+}
+
+cardButtons.forEach((button) =>
+  button.addEventListener('click', handleCardButtonClick)
+);
+
+function closeModal() {
+  modalOuter.classList.remove('open');
+}
+
+modalOuter.addEventListener('click', function (event) {
+  const isOutside = !event.target.closest('.modal-inner');
+  if (isOutside) {
+    closeModal();
+  }
+});
+
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    closeModal();
+  }
+});

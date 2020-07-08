7f452eb535b5e5d2b2a5acdb513699604a3d88dd
Kong Yang <kmongy05@gmail.com>
2020-07-08 11:30:20 -0500

    updated with logic for intersection observer.

---
+++ scrollEvents/main.js
@@ -0,0 +1,18 @@
+const terms = document.querySelector('.terms-and-conditions');
+const watch = document.querySelector('.watch');
+const button = document.querySelector('.accept');
+
+function obCallback(payload) {
+  if (payload[0].intersectionRatio === 1) {
+    button.disabled = false;
+    // stop observing the button
+    ob.unobserve(terms.lastElementChild);
+  }
+}
+
+const ob = new IntersectionObserver(obCallback, {
+  root: terms,
+  threshold: 1, // tel us when you're 100% on the page
+});
+
+ob.observe(terms.lastElementChild);

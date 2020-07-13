b4e6681875b8a297efea857b49ced35aface368e
Kong Yang <kmongy05@gmail.com>
2020-07-13 11:43:58 -0500

    updated html spelling errors. Updated minor css scaling. Updated with js logic.

---
+++ slider/slider.js
@@ -0,0 +1,59 @@
+function Slider(slider) {
+  if (!(slider instanceof Element)) {
+    throw new Error('No Slider passed in');
+  }
+  let prev;
+  let current;
+  let next;
+
+  const slides = slider.querySelector('.slides');
+  const prevButton = slider.querySelector('.goToPrev');
+  const nextButton = slider.querySelector('.goToNext');
+
+  function startSlider() {
+    current = slider.querySelector('.current') || slides.firstElementChild;
+    prev = current.previousElementSibling || slides.lastElementChild;
+    next = current.nextElementSibling || slides.firstElementChild;
+  }
+
+  function applyClasses() {
+    current.classList.add('current');
+    prev.classList.add('prev');
+    next.classList.add('next');
+  }
+
+  function move(direction) {
+    const classesToRemove = ['prev', 'current', 'next'];
+
+    prev.classList.remove(...classesToRemove);
+    current.classList.remove(...classesToRemove);
+    next.classList.remove(...classesToRemove);
+
+    if (direction === 'back') {
+      // destructure into new array
+      [prev, current, next] = [
+        prev.previousElementSibling || slides.lastElementChild,
+        prev,
+        current,
+      ];
+    } else {
+      [prev, current, next] = [
+        current,
+        next,
+        next.nextElementSibling || slides.firstElementChild,
+      ];
+    }
+
+    applyClasses();
+  }
+
+  startSlider();
+  applyClasses();
+
+  //Event listeners
+  prevButton.addEventListener('click', () => move('back'));
+  nextButton.addEventListener('click', () => move);
+}
+
+const slider1 = Slider(document.querySelector('.slider'));
+const slider2 = Slider(document.querySelector('.slider2'));

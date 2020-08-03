c97174b57badf440bb0d6e683ad05657b05bee0c
Kong Yang <kmongy05@gmail.com>
2020-08-03 14:42:21 -0500

    updated with js logic.

---
+++ APIDadjokes/main.js
@@ -0,0 +1,48 @@
+const jokeButton = document.querySelector('.getJoke');
+const jokeHolder = document.querySelector('.joke p');
+const loader = document.querySelector('.loader');
+
+const buttonText = [
+  'Ugh',
+  '🤦',
+  'omg dad',
+  'you are the worst',
+  'seriously?',
+  'stop it',
+  'please stop',
+  'that was the worst one',
+];
+
+async function fetchJoke() {
+  loader.classList.remove('hidden');
+  jokeButton.classList.add('hidden');
+  const response = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      Accept: 'application/json',
+    },
+  });
+  const data = await response.json();
+  loader.classList.add('hidden');
+  jokeButton.classList.remove('hidden');
+  return data;
+}
+
+function randomItemFromArray(arr, not) {
+  const item = arr[Math.floor(Math.random() * arr.length)];
+  if (item === not) {
+    console.log('we used this one');
+    return randomItemFromArray(arr, not);
+  }
+  return item;
+}
+
+async function handleClick() {
+  const { joke } = await fetchJoke();
+  jokeHolder.textContent = joke;
+  jokeButton.textContent = randomItemFromArray(
+    buttonText,
+    jokeButton.textContent
+  );
+}
+
+jokeButton.addEventListener('click', handleClick);

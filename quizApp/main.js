3f40b32e176384845ced85782e364b6b7d92f4c2
Kong Yang <kmongy05@gmail.com>
2020-09-08 16:32:32 -0500

    Initial commit of quiz app.

---
+++ quizApp/main.js
@@ -0,0 +1,11 @@
+const questionAndAnswers = [
+  {
+    question: 'What is my favorite food?',
+    a: 'Tacos',
+    b: 'Lobster',
+    c: 'Pho',
+    d: 'Bahn Mi Sandwich',
+    e: 'All of the above',
+    correct: 'c',
+  },
+];

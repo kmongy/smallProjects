42bfbd0f840dd8091939613126766f3829cc31eb
Kong Yang <kmongy05@gmail.com>
2020-09-09 11:51:39 -0500

    updated with new content.

--- quizApp/main.js
+++ quizApp/main.js
@@ -1,11 +1,53 @@
 const questionAndAnswers = [
   {
-    question: 'What is my favorite food?',
-    a: 'Tacos',
-    b: 'Lobster',
-    c: 'Pho',
-    d: 'Bahn Mi Sandwich',
-    e: 'All of the above',
+    question: 'What year was Jackie Chan born?',
+    a: '1968',
+    b: '1954',
+    c: '1970',
+    d: '1929',
+    correct: 'b',
+  },
+  {
+    question: 'Jackie Chan trained for jingxi for how many years?',
+    a: '15',
+    b: '8',
+    c: '12',
+    d: '10',
+    correct: 'd',
+  },
+  {
+    question: 'In addition to acting, Jackie also has a career in...',
+    a: 'Computer Science',
+    b: 'Sports',
+    c: 'Music',
+    d: 'Architecture',
     correct: 'c',
+  },
+  {
+    question:
+      'Jackie Chan helped to transform the Hong Kong movie scene with his production company called...',
+    a: 'Drunken Master',
+    b: "Eagle's Shadow",
+    c: 'Golden Harvest',
+    d: 'Fearless Hyena',
+    correct: 'b',
+  },
+  {
+    question:
+      'Doing his own dangerous stunts, Jackie nearly perished in what movie?',
+    a: 'Project A',
+    b: 'Police Story',
+    c: 'The Young Master',
+    d: 'Armour of God',
+    correct: 'd',
+  },
+  {
+    question:
+      'In 2016, Chan became the first Chinese actor to receive an honorary _____ Award',
+    a: 'Academy',
+    b: 'Tony',
+    c: 'MTV',
+    d: 'Globe',
+    correct: 'a',
   },
 ];
